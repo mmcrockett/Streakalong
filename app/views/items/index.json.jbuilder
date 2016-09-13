6547848568e8bf146a4280af0f7b3b1c6c11b669
Mike Crockett <github@mmcrockett.com>
2016-09-12 21:00:00 -0500

    Fixing issue with asset pipeline rendering images.

--- app/views/items/index.json.jbuilder
+++ app/views/items/index.json.jbuilder
@@ -1,3 +1,4 @@
 json.array!(@items) do |item|
   json.extract! item, :id, :name, :kcal
+  json.image_src(asset_path("items/#{item[:name]}.png"))
 end
--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class ItemsControllerTest < ActionController::TestCase
   test "json index should get item data" do
+    image_key = "image_src"
     request_json
     get :index
 
@@ -12,6 +13,14 @@ class ItemsControllerTest < ActionController::TestCase
     end
 
     assert_response :success
-    assert_equal(JSON.parse(expected.to_json), JSON.parse(@response.body))
+    items_returned = JSON.parse(@response.body)
+
+    items_returned.each do |item|
+      assert(item[image_key].include?("items"))
+      assert(item[image_key].include?("#{item[:name]}.png"))
+      item.delete(image_key)
+    end
+
+    assert_equal(JSON.parse(expected.to_json), items_returned)
   end
 end
